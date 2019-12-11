1541964888c91035e2811bd2a31996cad0cefe47
gowthamsabbineni <43555811+gowthamsabbineni@users.noreply.github.com>
2019-12-11 18:38:36 -0500

    changes for heroku deployment

--- ProjectManagerApp/server.js
+++ ProjectManagerApp/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 require('dotenv').config();
 const app = express();
 app.use(cors());
@@ -27,6 +28,16 @@ app.use('/employees',employeeRoutes);
 const authRoutes = require('./routes/authenticationRoutes');
 app.use('/auth',authRoutes);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 app.listen(port,()=>{
     console.log('app is listening to port '+ port);
 })
